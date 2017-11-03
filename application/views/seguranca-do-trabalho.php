031e44d823eabd895c930e266b82c5f4cd51314c
Caio Batista <cbatista.93@gmail.com>
2017-11-03 00:19:03 -0200

    pagina seguranca do trabalho

--- application/views/seguranca-do-trabalho.php
+++ application/views/seguranca-do-trabalho.php
@@ -17,20 +17,32 @@
     <!-- Image Header -->
     <div class="row">
         <div class="col-lg-12">
-            <img class="img-responsive" src="http://placehold.it/1200x300" alt="">
+            <img class="img-responsive" src="public/img/segtrab.png" alt="">
         </div>
     </div>
     <!-- /.row -->
 
     <div class="row margin-top-30">
         <div class="col-lg-12">
-            <h2 class="page-header">Service List</h2>
+            <h2 class="page-header">NR's <small>(Normas Regulamentadoras)</small></h2>
         </div>
         <div class="col-md-2"></div>
         <div class="col-md-8 font-1-2">
             <p>
-            A STQ TREINAMENTOS está altamente qualificada para uma completa consultoria em Segurança do Trabalho, onde realizamos treinamentos com todas as NR’s, acompanhamos perícias trabalhistas, e promovemos uma melhoria contínua, ajudando o nosso cliente a trabalhar com a prevenção.
+                Completa consultoria em <b>Segurança do Trabalho</b>, com realização de treinamentos com todas as <b>NR’s</b>, acompanhamento de perícias trabalhistas e promoção de melhoria contínua, ajudando o nosso cliente a trabalhar com a prevenção de acordo com a estrutura do negócio.
+                <br /><br />
+                As <b>Normas Regulamentadoras</b> (NR's) são de observância obrigatória pelas empresas privadas e públicas e pelos orgãos públicos de administração direta e indireta, que possuam empregados regidos pela <b>Consolidação das Leis do Trabalho - CLT</b>. Estabelece a importância, funções e competência da <b>Delegacia Regional do Trabalho</b>.
             </p>
+            <br /><br />
+            <ul class="work__nr-list">
+                <li><b>NR 01</b> - Disposições Gerais</li>
+                <li><b>NR 04</b> - Serviços Especializados em Eng. de Segurança e em Medicina do Trabalho</li>
+                <li><b>NR 05</b> - Comissão Interna de Prevenção de Acidentes</li>
+                <li><b>NR 06</b> - Uso, Conservação e descartes de E.P.I’s e E.P.C’s</li>
+                <li><b>NR 07</b> - Programas de Controle Médico de Saúde Ocupacional</li>
+                <li><b>NR 09</b> - Programas de Prevenção de Riscos Ambientais</li>
+                <li><b>NR 10</b> - Segurança em Instalações e Serviços em Eletricidade</li>
+            </ul>
         </div>
         <div class="col-md-2"></div>
     </div>
